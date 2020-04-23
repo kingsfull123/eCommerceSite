fdb0dfccca185f674997b78a0cbbd91154c79f91
jason yang <syan19@live.cn>
2020-04-23 15:22:40 +1000

    shoes displayed by JS

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+// Display 10 basketball shoes using JavaScript
+const productsContainer = document.querySelector(".products-container");
 
+const products = document.createElement("div");
+products.className = "products";
+products.style.cssText =
+  "width: 90%;height: auto;margin: 0 auto;padding: 30px 0;display: grid;grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));column-gap: 30px;row-gap: 15px;";
+
+for (let i = 0; i < data.length; i++) {
+  const productContainer = document.createElement("div");
+  productContainer.className = "product-container";
+  const productImg = document.createElement("img");
+  productImg.className = "product-img";
+
+  const productDescription = document.createElement("h4");
+  productDescription.className = "product-description";
+
+  const productPrice = document.createElement("h5");
+  productPrice.className = "product-price";
+  productImg.src = data[i].src;
+  productDescription.textContent = data[i].description;
+  productPrice.textContent = data[i].price;
+
+  productContainer.appendChild(productImg);
+  productContainer.appendChild(productDescription);
+  productContainer.appendChild(productPrice);
+
+  products.appendChild(productContainer);
+}
+
+productsContainer.appendChild(products);
---
+++ data.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    id: 1,
+    description: "custom-kyrie-6-by-you",
+    price: 145,
+    src: "./images/products/custom-kyrie-6-by-you.jpg",
+  },
+  {
+    id: 2,
+    description: "kyrie-6-enlightenment",
+    price: 178,
+    src: "./images/products/kyrie-6-enlightenment.jpg",
+  },
+  {
+    id: 3,
+    descrption: "Kyrie-low-by-you-Basketball-Shoe",
+    price: 245,
+    src: "./images/products/Kyrie-low-by-you-Basketball-Shoe.jpg",
+  },
+  {
+    id: 4,
+    description: "lebron-17-win-win-basketball-shoe",
+    price: 350,
+    src: "./images/products/lebron-17-win-win-basketball-shoe.jpg",
+  },
+  {
+    id: 5,
+    description: "Nike-Hyperdunk-X-Basketball-Shoe",
+    price: 245,
+    src: "./images/products/Nike-Hyperdunk-X-Basketball-Shoe.jpg",
+  },
+  {
+    id: 6,
+    description: "PG3",
+    price: 125,
+    src: "./images/products/PG3.jpg",
+  },
+  {
+    id: 7,
+    description: "zoom-freak-1-basketball-shoe",
+    price: 240,
+    src: "./images/products/zoom-freak-1-basketball-shoe.jpg",
+  },
+  {
+    id: 8,
+    description: "zoom-freak-1-multi-basketball-shoe",
+    price: 145,
+    src: "./images/products/air-max-impact-basketball-shoe.jpg",
+  },
+  {
+    id: 9,
+    description: "air-max-impact-basketball-shoe",
+    price: 145,
+    src: "./images/products/air-max-impact-basketball-shoe.jpg",
+  },
+  {
+    id: 10,
+    description: "kyrie-6-ep-basketball-shoe",
+    price: 145,
+    src: "./images/products/kyrie-6-ep-basketball-shoe.jpg",
+  },
+];
